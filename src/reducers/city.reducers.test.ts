b9e6cb05acd816a3d2aa212384616a6d3215d5b1
ekta-r <rathiekta2801@gmail.com>
2024-02-19 12:50:26 +0000

    Code formatting

--- src/reducers/city.reducers.test.ts
+++ src/reducers/city.reducers.test.ts
@@ -2,12 +2,12 @@ import * as CityNameReducerAction from "./city.reducers";
 import store from "../../store.ts";
 
 describe("Name Reducer", () => {
-    it("sets selected name", async () => {
-        const result = await store.dispatch(
-            CityNameReducerAction.setCityName("London")
-        );
-        expect(result.payload).toEqual("London");
-        const state = store.getState().name.cityName;
-        expect(state).toEqual("London");
-      });
+  it("sets selected name", async () => {
+    const result = await store.dispatch(
+      CityNameReducerAction.setCityName("London")
+    );
+    expect(result.payload).toEqual("London");
+    const state = store.getState().name.cityName;
+    expect(state).toEqual("London");
+  });
 });
