af97dff7fa620ab18b7029d8aaffb447e8b96220
ekta-r <rathiekta2801@gmail.com>
2024-02-19 12:50:47 +0000

    Code formatting axios handler

--- src/utils/axiosHandler.ts
+++ src/utils/axiosHandler.ts
@@ -2,23 +2,28 @@ import axios from "axios";
 
 // Request interceptors for API calls
 axios.interceptors.request.use(
-    (config) => {
-      config.headers["Content-Type"] = "application/json";
-      config.headers["accept"] = "application/json";
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
-  
-  // Response interceptors for API calls
-  axios.interceptors.response.use((response) => response, (error) => {
+  (config) => {
+    config.headers["Content-Type"] = "application/json";
+    config.headers["accept"] = "application/json";
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptors for API calls
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
     if (error.response.status === 404) {
-        window.alert("Invalid City");
+      window.alert("Invalid City");
     } else if (error.response.status === 401) {
-      window.alert("Invalid API key. Please see https://openweathermap.org/faq#error401 for more info. ")
+      window.alert(
+        "Invalid API key. Please see https://openweathermap.org/faq#error401 for more info. "
+      );
     }
-  });
-  
-  export default axios;
+  }
+);
+
+export default axios;
